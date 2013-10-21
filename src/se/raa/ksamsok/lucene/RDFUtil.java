677ae62a8f9b52aa27451f03094473d255e7d500
alwik <alwik>
2013-10-21 08:25:28 +0000

    #3821, #3822

--- src/se/raa/ksamsok/lucene/RDFUtil.java
+++ src/se/raa/ksamsok/lucene/RDFUtil.java
@@ -9,78 +9,75 @@ import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.jrdf.JRDFFactory;
-import org.jrdf.SortedMemoryJRDFFactory;
-import org.jrdf.collection.MemMapFactory;
-import org.jrdf.graph.AnyObjectNode;
-import org.jrdf.graph.AnySubjectNode;
-import org.jrdf.graph.Graph;
-import org.jrdf.graph.GraphElementFactory;
-import org.jrdf.graph.Literal;
-import org.jrdf.graph.PredicateNode;
-import org.jrdf.graph.SubjectNode;
-import org.jrdf.graph.Triple;
-import org.jrdf.graph.URIReference;
-import org.jrdf.parser.rdfxml.GraphRdfXmlParser;
+
+import com.hp.hpl.jena.rdf.model.Literal;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Selector;
+import com.hp.hpl.jena.rdf.model.SimpleSelector;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 
 public class RDFUtil {
 
 	private static final Logger logger = Logger.getLogger(RDFUtil.class);
 
-    // har en close() men den gör inget så vi skapar bara en instans
-	private static final JRDFFactory jrdfFactory = SortedMemoryJRDFFactory.getFactory();
-
-	public static Graph parseGraph(String rdfXml) throws Exception {
-		Graph graph;
+    public static Model parseModel(String rdfXml) throws Exception {
+		Model model;
 		StringReader r = null;
 		try {
 			r = new StringReader(rdfXml);
-			graph = parseGraph(r);
+			model = parseModel(r);
 		} finally {
 			if (r != null) {
 				r.close();
 			}
 		}
-		return graph;
+		return model;
 	}
 
-	public static Graph parseGraph(Reader r) throws Exception {
-		Graph graph = jrdfFactory.getNewGraph();
-		GraphRdfXmlParser parser = new GraphRdfXmlParser(graph, new MemMapFactory());
-		parser.parse(r, "");
-		return graph;
+	public static Model parseModel(Reader r) throws Exception {
+		Model m = ModelFactory.createDefaultModel();
+		m.read(r, "");
+		return m;
 	}
 
 	// läser ut ett värde ur subjektnoden eller subjektnodens objektnod om denna är en subjektnod
 	// och lägger till värdet mha indexprocessorn
-	static String extractValue(Graph graph, SubjectNode s, URIReference ref, URIReference refRef, IndexProcessor ip) throws Exception {
-		return extractValue(graph, s, ref, refRef, ip, null);
+	static String extractValue(Model model, Resource subject, Property ref, Property refRef, IndexProcessor ip) throws Exception {
+		return extractValue(model, subject, ref, refRef, ip, null);
 	}
 
 	// läser ut ett värde ur subjektnoden eller subjektnodens objektnod om denna är en subjektnod
 	// och lägger till värdet mha indexprocessorn och ev specialhanterar relationer
-	static String extractValue(Graph graph, SubjectNode s, URIReference ref, URIReference refRef, IndexProcessor ip, List<String> relations) throws Exception {
+	static String extractValue(Model model, Resource subject, Property ref, Property refRef, IndexProcessor ip, List<String> relations) throws Exception {
 		final String sep = " ";
 		StringBuffer buf = new StringBuffer();
 		String value = null;
-		for (Triple t: graph.find(s, ref, AnyObjectNode.ANY_OBJECT_NODE)) {
-			if (t.getObject() instanceof Literal) {
-				Literal l = (Literal) t.getObject();
+		Selector selector = new SimpleSelector(subject, ref, (RDFNode) null);
+		StmtIterator iter = model.listStatements(selector);
+		while (iter.hasNext()){
+			Statement s = iter.next();
+			if (s.getObject().isLiteral()){
 				if (buf.length() > 0) {
 					buf.append(sep);
 				}
-				value = l.getValue().toString();
+				value = s.getObject().asLiteral().getString();
 				buf.append(value);
 				if (ip != null) {
 					ip.addToDoc(value);
 					// specialfall för att hantera relationer som nog egentligen felaktigt(?) är vanliga värden, de borde vara rdf-resurser och då URIReferences
 					// jmfr <isPartOf>http...</isPartOf> och <isPartOf rdf:resource="http..." />
 					if (relations != null) {
-						ip.lookupAndHandleURIValue(value, relations, ref != null ? ref.getURI().toString() : null);
+						ip.lookupAndHandleURIValue(value, relations, ref != null ? ref.getURI() : null);
 					}
 				}
-			} else if (t.getObject() instanceof URIReference) {
-				value = getReferenceValue((URIReference) t.getObject(), ip, relations, ref);
+			} else if (s.getObject().isURIResource()){
+				value = getReferenceValue(s.getObject().asResource(), ip, relations, ref);
 				// lägg till i buffer bara om detta är en uri vi ska slå upp värde för
 				if (value != null && ip != null && ip.translateURI()) {
 					if (buf.length() > 0) {
@@ -91,9 +88,8 @@ public class RDFUtil {
 				if (ip != null) {
 					ip.addToDoc(value);
 				}
-			} else if (refRef != null && t.getObject() instanceof SubjectNode) {
-				SubjectNode resSub = (SubjectNode) t.getObject();
-				value = extractSingleValue(graph, resSub, refRef, ip);
+			} else if (refRef != null && s.getObject().isResource()){
+				value = extractSingleValue(model, s.getObject().asResource(), refRef, ip);
 				if (value != null) {
 					if (buf.length() > 0) {
 						buf.append(sep);
@@ -101,34 +97,6 @@ public class RDFUtil {
 					buf.append(value);
 				}
 			}
-//			if (value != null && ip != null && relations != null) {
-//				ip.handleRelation(ref, value, relations);
-//			}
-//			if (value != null && relations != null) {
-//				// specialhantering av relationer
-//				String relationType;
-//				if (SamsokProtocol.uri_rSameAs.equals(ref.getURI())) {
-//					relationType = "sameAs";
-//				} else {
-//					relationType = StringUtils.trimToNull(StringUtils.substringAfter(ref.getURI().toString(), uriPrefixKSamsok));
-//					// TODO: fixa bättre
-//					if (relationType == null) {
-//						// testa cidoc
-//						relationType = StringUtils.trimToNull(StringUtils.substringAfter(ref.getURI().toString(), uriPrefix_cidoc_crm));
-//						if (relationType != null) {
-//							// strippa sifferdelen
-//							relationType = StringUtils.trimToNull(StringUtils.substringAfter(relationType, "."));
-//						} else {
-//							// bios
-//							relationType = StringUtils.trimToNull(StringUtils.substringAfter(ref.getURI().toString(), uriPrefix_bio));
-//						}
-//					}
-//					if (relationType == null) {
-//						throw new Exception("Okänd relation? Börjar ej med känt prefix: " + ref.getURI().toString());
-//					}
-//				}
-//				relations.add(relationType + "|" + value);
-//			}
 			value = null;
 		}
 		return buf.length() > 0 ? StringUtils.trimToNull(buf.toString()) : null;
@@ -136,18 +104,21 @@ public class RDFUtil {
 
 	// läser ut ett enkelt värde ur subjektoden där objektnoden måste vara en literal eller en uri-referens
 	// och lägger till det mha indexprocessorn
-	static String extractSingleValue(Graph graph, SubjectNode s, PredicateNode p, IndexProcessor ip) throws Exception {
+	static String extractSingleValue(Model model, Resource subject, Property p, IndexProcessor ip) throws Exception {
 		String value = null;
-		for (Triple t: graph.find(s, p, AnyObjectNode.ANY_OBJECT_NODE)) {
+		Selector selector = new SimpleSelector(subject, p, (RDFNode) null);
+		StmtIterator iter = model.listStatements(selector);
+		while (iter.hasNext()){
 			if (value != null) {
-				throw new Exception("Fler värden än ett för s: " + s + " p: " + p);
+				throw new Exception("Fler värden än ett för s: " + subject + " p: " + p);
 			}
-			if (t.getObject() instanceof Literal) {
-				value = StringUtils.trimToNull(((Literal) t.getObject()).getValue().toString());
-			} else if (t.getObject() instanceof URIReference) {
-				value = getReferenceValue((URIReference) t.getObject(), ip, null, null); // TODO: handle relations?
-			} else {
-				throw new Exception("Måste vara literal/urireference o.class: " + t.getObject().getClass().getSimpleName() + " för s: " + s + " p: " + p);
+			Statement s = iter.next();
+			if (s.getObject().isLiteral()){
+				value = StringUtils.trimToNull(s.getObject().asLiteral().getString());
+			} else if (s.getObject().isURIResource()){
+				value = getReferenceValue(s.getObject().asResource(), ip, null, null);
+			}else {
+				throw new Exception("Måste vara literal/urireference o.class: " + s.getObject().getClass().getSimpleName() + " för s: " + subject + " p: " + p);
 			}
 			if (ip != null) {
 				ip.addToDoc(value);
@@ -157,10 +128,10 @@ public class RDFUtil {
 	}
 
 	// försöker översätta ett uri-värde till ett förinläst värde
-	private static String getReferenceValue(URIReference object, IndexProcessor ip, List<String> relations, URIReference ref) throws Exception {
+	private static String getReferenceValue(Resource object, IndexProcessor ip, List<String> relations, Property ref) throws Exception {
 		String value = null;
-		String uri = object.getURI().toString();
-		String refUri = ref != null ? ref.getURI().toString() : null;
+		String uri = object.getURI();
+		String refUri = ref != null ? ref.getURI() : null;
 		// se om vi ska försöka ersätta uri:n med en uppslagen text
 		// och lägga in den i relations
 		if (ip != null) { // && ip.translateURI()) {
@@ -188,19 +159,19 @@ String value = null;
 	// alla resurser förutsätts vara kodade i utf-8 och att värdena är Literals
 	static void readURIValueResource(String fileName, URI predicateURI, Map<String, String> uriValues) {
 		Reader r = null;
-		Graph graph = null;
+		Model model = null;
 		try {
 			r = new InputStreamReader(RDFUtil.class.getResourceAsStream(fileName), "UTF-8");
-			graph = parseGraph(r);
-			GraphElementFactory elementFactory = graph.getElementFactory();
-			URIReference rName = elementFactory.createURIReference(predicateURI);
-			int c = 0;
-			for (Triple t: graph.find(AnySubjectNode.ANY_SUBJECT_NODE, rName, AnyObjectNode.ANY_OBJECT_NODE)) {
-				uriValues.put(t.getSubject().toString(), StringUtils.trimToNull(((Literal) t.getObject()).getValue().toString()));
-				++c;
+			model = parseModel(r);
+			Property rName = ResourceFactory.createProperty(predicateURI.toString());
+			Selector selector = new SimpleSelector((Resource) null, rName, (RDFNode) null);
+			StmtIterator iter = model.listStatements(selector);
+			while (iter.hasNext()){
+				Statement s = iter.next();
+				uriValues.put(s.getResource().toString(), StringUtils.trimToNull(s.getObject().asLiteral().getString()));
 			}
 			if (logger.isInfoEnabled()) {
-				logger.info("Läste in " + c + " uris/värden från " + fileName);
+				logger.info("Läste in " + iter.toList().size() + " uris/värden från " + fileName);
 			}
 		} catch (Exception e) {
 			throw new RuntimeException("Problem att läsa in uri-översättningsfil " +
@@ -212,8 +183,8 @@ String value = null;
 				} catch (Exception ignore) {
 				}
 			}
-			if (graph != null) {
-				graph.close();
+			if (model != null) {
+				model.close();
 			}
 		}
 	}
