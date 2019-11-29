8a1584d928d987d99dec2d671b400e1636fb6111
Stefan Malmesjö <stefan.malmesjo@altrusoft.se>
2019-11-29 12:04:40 +0100

    KSAM-349 Some tests and corrected a couple of bugs discovered by the tests

--- src/main/java/se/raa/ksamsok/lucene/TimeUtil.java
+++ src/main/java/se/raa/ksamsok/lucene/TimeUtil.java
@@ -30,20 +30,30 @@ public class TimeUtil {
 		String result = null;
 		try {
 			// tills vidare godkänner vi också "x f.kr" och "y e.kr" skiftlägesokänsligt här
-			int dotkrpos = value.toLowerCase().indexOf(".kr");
+			value = value.toLowerCase();
+			int dotkrpos = value.indexOf(".kr");
 			int year;
 			if (dotkrpos > 0) {
 				year = Integer.parseInt(value.substring(0, dotkrpos - 1).trim());
+				boolean canBeNegative = false;
 				final char character = value.charAt(dotkrpos - 1);
 				switch (character) {
-				case 'F':
 				case 'f':
-					year = -year + 1; // 1 fkr är år 0
+					// vi godtar inte 0 f.Kr (eller negativa år före kristus)
+					if (year > 0 ) {
+						// TODO: fundera på om 1 före kristus verkligen ska vara år 0 - år 0 finns väl inte?
+						year = -year + 1; // 1 fkr är år 0
+						canBeNegative = true;
+					} else {
+						throw new NumberFormatException();
+					}
 					// obs - inget break
-				case 'E':
 				case 'e':
 					// TODO: "numerfixen" görs numera på solr-sidan, ändra returtyp?
-					result = String.valueOf(year);
+					// här måste året vara icke-negativt (men 0 är ok)
+					if (canBeNegative || year >= 0) {
+						result = String.valueOf(year);
+					}
 					//result = transformNumberToLuceneString(year);
 					break;
 					default:
