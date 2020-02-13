5405b34ed5dfc1edb7acc5827e16f4460a1076f4
allar <albin.larsson@raa.se>
2020-02-13 12:51:57 +0100

    remove old unused SQL files
    Moved to another repository

--- sql/patch_1_1_2-02.sql
+++
@@ -1,5 +0,0 @@
--- patch-fil för att uppdatera en tabell för att kunna pausa en/flera skördning/ar
-ALTER TABLE harvestservices ADD COLUMN paused boolean;
-
-UPDATE harvestservices
-SET paused = 'f'
