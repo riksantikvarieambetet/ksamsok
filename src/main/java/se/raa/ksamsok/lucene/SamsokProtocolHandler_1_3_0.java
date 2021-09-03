380d7bf64cf7173748ad522fceb8df4ca0714bf9
stmal <stefan.malmesjo@raa.se>
2021-09-03 17:17:42 +0200

    KSAM-439
    URI:er för de nya indexen ska inte slås upp utan indexeras såsom de är.

--- src/main/java/se/raa/ksamsok/lucene/SamsokProtocolHandler_1_3_0.java
+++ src/main/java/se/raa/ksamsok/lucene/SamsokProtocolHandler_1_3_0.java
@@ -21,20 +21,20 @@ public class SamsokProtocolHandler_1_3_0 extends SamsokProtocolHandler_1_2_0 {
     protected void extractContextTimeInformation(Resource cS, String[] contextTypes) throws Exception {
         super.extractContextTimeInformation(cS, contextTypes);
 
-        ip.setCurrent(IX_FROMPERIOD, contextTypes);
+        ip.setCurrent(IX_FROMPERIOD, contextTypes, false, null);
         extractValue(model, cS, getURIRef(uri_rFromPeriod), ip);
 
-        ip.setCurrent(IX_TOPERIOD, contextTypes);
+        ip.setCurrent(IX_TOPERIOD, contextTypes, false, null);
         extractValue(model, cS, getURIRef(uri_rToPeriod), ip);
 
-        ip.setCurrent(IX_EVENT, contextTypes);
+        ip.setCurrent(IX_EVENT, contextTypes, false, null);
         extractValue(model, cS, getURIRef(uri_rEvent), ip);
     }
 
     protected void extractContextPlaceInformation(Resource cS, String[] contextTypes, List<String> gmlGeometries) throws Exception {
         super.extractContextPlaceInformation(cS, contextTypes, gmlGeometries);
 
-        ip.setCurrent(IX_PLACETERM, contextTypes);
+        ip.setCurrent(IX_PLACETERM, contextTypes, false, null);
         extractValue(model, cS, getURIRef(uri_rPlaceTerm), ip);
 
     }
@@ -42,7 +42,7 @@ public class SamsokProtocolHandler_1_3_0 extends SamsokProtocolHandler_1_2_0 {
     protected void extractContextActorInformation(Resource cS, String[] contextTypes, List<String> relations) throws Exception {
         super.extractContextActorInformation(cS, contextTypes, relations);
 
-        ip.setCurrent(IX_AGENT, contextTypes);
+        ip.setCurrent(IX_AGENT, contextTypes, false, null);
         extractValue(model, cS, getURIRef(uri_rAgent), ip);
     }
 
