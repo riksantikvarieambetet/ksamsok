44de65f20d9faa3e14e2db63e185635c5f424c1f
Stefan Malmesjö <stefan.malmesjo@firebric.se>
2022-05-17 14:40:40 +0200

    KSAM-496 Kommentar i filen

--- conf/setenv.sh
+++ conf/setenv.sh
@@ -4,5 +4,6 @@
 export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
+# Används javas interna TransformerFactory, som hanterar emoticons i xml bättre än vad xalan gör
 #CATALINA_OPTS
 export CATALINA_OPTS="-d64 -Djavax.xml.transform.TransformerFactory=com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl -Dsun.net.client.defaultReadTimeout=5000000 -Dsun.net.client.defaultConnectTimeout=60000 -Djava.awt.headless=true -Dkms.ojb.lrucache.size=100000"
