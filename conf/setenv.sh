fbca77016f9b1a0b7676bb4bb82c1176fee5dbed
Stefan Malmesjö <stefan.malmesjo@firebric.se>
2022-05-17 14:39:50 +0200

    KSAM-496 Använd javas interna TransformerFactory. Den hanterar höga Unicode bättre än xalans gör

--- conf/setenv.sh
+++ conf/setenv.sh
@@ -5,4 +5,4 @@ export LC_ALL=en_US.utf-8
 export LANG=en_US.utf-8
 
 #CATALINA_OPTS
-export CATALINA_OPTS="-d64 -Dsun.net.client.defaultReadTimeout=5000000 -Dsun.net.client.defaultConnectTimeout=60000 -Djava.awt.headless=true -Dkms.ojb.lrucache.size=100000"
+export CATALINA_OPTS="-d64 -Djavax.xml.transform.TransformerFactory=com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl -Dsun.net.client.defaultReadTimeout=5000000 -Dsun.net.client.defaultConnectTimeout=60000 -Djava.awt.headless=true -Dkms.ojb.lrucache.size=100000"
